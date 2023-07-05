9006be943b490af73859e0f9a7ca7bd9156b0c8f
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-07-05 11:05:35 -0700

    Fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 import './modules/countries.js';
-import './modules/likeitems.js';
 import './modules/reservation.js';
 import './modules/popup.js';
+import './modules/likeitems.js';
 
