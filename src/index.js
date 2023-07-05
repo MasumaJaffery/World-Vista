09a31df07ef949cc3a006897fb92414225f39deb
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-07-05 23:08:12 +0430

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import './style.css';
 import './modules/countries.js';
 import './modules/reservation.js';
 import './modules/commentpop.js';
-import './modules/popup.js';
 import './modules/likeitems.js';
-
