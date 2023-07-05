4f4719b360aa6e95f5894c867563f3aab29a41a7
MSA <crystalbluetech@gmail.com>
2023-07-05 10:03:22 +0100

    create pop-up when click on reservation button

--- src/modules/reservation.js
+++ src/modules/reservation.js
@@ -1,36 +1,28 @@
 const createReservationForm = () => {
-    // Create the popup container
     const popupContainer = document.createElement('div');
     popupContainer.className = 'popup';
 
-    // Create the form container
     const formContainer = document.createElement('div');
     formContainer.className = 'form-container';
 
-    // Create the h1 element
     const title = document.createElement('h1');
     title.textContent = 'Add a Reservation';
 
-    // Create the name input
     const nameInput = document.createElement('input');
     nameInput.type = 'text';
     nameInput.placeholder = 'Your Name';
 
-    // Create the start date input
     const startDateInput = document.createElement('input');
     startDateInput.type = 'date';
     startDateInput.placeholder = 'Start Date';
 
-    // Create the end date input
     const endDateInput = document.createElement('input');
     endDateInput.type = 'date';
     endDateInput.placeholder = 'End Date';
 
-    // Create the reserve button
     const reserveButton = document.createElement('button');
     reserveButton.textContent = 'Reserve';
     reserveButton.addEventListener('click', async () => {
-        // Reservation submission code...
     });
 
     // Append the elements to the form container
@@ -40,10 +32,8 @@ const createReservationForm = () => {
     formContainer.appendChild(endDateInput);
     formContainer.appendChild(reserveButton);
 
-    // Append the form container to the popup container
     popupContainer.appendChild(formContainer);
 
-    // Append the popup container to the body
     document.body.appendChild(popupContainer);
 };
 
