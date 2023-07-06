c059a0c512364ea83f874cdbf4f0efee68a5db15
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-07-06 04:04:06 -0700

    Liked Added

--- src/modules/countries.js
+++ src/modules/countries.js
@@ -1,40 +1,48 @@
-const getcountry = async() => {
-    try {
-      const response = await fetch('https://restcountries.com/v3.1/all');
-      const data = await response.json();
-      data.forEach(item => {
-        postcountry(item);
-      });
-    } catch (error) {
-      console.log('Error');
-    }
-  }  
-  getcountry();
+const getcountry = async () => {
+  try {
+    const response = await fetch('https://restcountries.com/v3.1/all');
+    const data = await response.json();
+    data.forEach((item) => {
+      postcountry(item);
+    });
+  } catch (error) {
+    console.log('Error');
+  }
+};
+getcountry();
 
-  const postcountry = async (data) => {
-    const countries = document.querySelector('.countries');
-    const country = document.createElement('div');
-    country.className = 'country';
-    country.innerHTML =
-    `<div class="card country" style="width: 20rem; height: 23.5rem;">
-     <img src=${data.flags.svg} class="card-img-top" alt="..."  style="object-fit: cover; width: 100%; height: 12rem;>
-     <div class="card-body">
-     <div class = "card-flex">
-     <h5>${data.name.common}</h5>
-     <div class = "icon-counter">
-     <button type = "button" class = "icon-btn"><i class="fa-regular fa-heart"></i>
-     </button>
-     <div class = "like">
-     <span class = "like-counter">0</span><span> like's</span></div>
-     </div>
-     </div>
-     <div class ="comment">
-     <button type = "button" class = "btn btn-comment btn-primary">Comment</button>
-     </div>
-     <div class ="reservation">
-     <button type = "button" class = "btn btn-reservation btn-success">Reservations</button>
-     </div>
-     </div>
-    </div>`;
-    countries.appendChild(country);
+const postcountry = async (data) => {
+  const countries = document.querySelector('.countries');
+  const country = document.createElement('div');
+  country.className = 'country';
+  country.innerHTML =
+  `<div class="card country" style="width: 20rem; height: 23.5rem;">
+   <img src=${data.flags.svg} class="card-img-top" alt="..."  style="object-fit: cover; width: 100%; height: 12rem;>
+   <div class="card-body">
+   <div class = "card-flex">
+   <h5>${data.name.common}</h5>
+   <div class = "icon-counter">
+   <button type = "button" class = "icon-btn"><i class="fa-regular fa-heart"></i>
+   </button>
+   <div class = "like">
+   <span class = "like-counter">0</span><span> like's</span></div>
+   </div>
+   </div>
+   <div class ="comment">
+   <button type = "button" class = "btn btn-comment btn-primary">Comment</button>
+   </div>
+   <div class ="reservation">
+   <button type = "button" class = "btn btn-reservation btn-success">Reservations</button>
+   </div>
+   </div>
+  </div>`;
+  countries.appendChild(country);
+  //itemcount() is callback func here!
+  itemcount();
+}
+// Countries Card Counter!
+const itemcount = async () => {
+const count = document.getElementById('count');
+const countries = document.querySelectorAll('.country');
+count.textContent = `(${countries.length}) `;
 }
--- src/modules/likeitems.js
+++ src/modules/likeitems.js
@@ -1,22 +1,56 @@
-import './countries.js';
-const appID = '0I1gsmjKECA6HGAxhWHM';
-const baseAPI = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
-const likeURL = `${baseAPI}/${appID}/likes`;
-const getlike = async() => {
-  const response = await fetch(likeURL);
-  const data = await response.json();
-  console.log(data);
-  const iconbtn = document.querySelectorAll('.icon-btn');
-  iconbtn.forEach((btn, index) => {
-    btn.addEventListener('click', async () => {
-      const likecounter = document.querySelectorAll('.like-counter');
-if (likecounter[index]) {
-  likecounter[index].innerHTML = parseInt(likecounter[index].innerHTML) + 1;
-  console.log('clicked');
-}
-//Send like to Api!
-  });
-});
+const countriesArray = []; // Array to store the country objects
+const involvementApiEndpoint = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/FjhFMUdws0lCxR3eXCdS/likes';
+const iconbtn = document.querySelector('.icon-btn');
+const likecounter = document.querySelector('.like-counter');
+// Fetching countries from REST Countries API
+fetch('https://restcountries.com/v2/all')
+  .then(response => response.json())
+  .then(data => {
+    // Looping through the fetched data and extracting the required information
+    data.forEach(country => {
+      const { name, capital, population, area } = country; // Customize the properties you need for each country
+      
+      // Creating a country object with the desired information
+      const countryObj = {
+        name,
+        capital,
+        population,
+        area
+      };
 
+      // Adding the country object to the array
+      countriesArray.push(countryObj);
+    });
+
+    // Looping through the countriesArray and sending like requests for each country
+    countriesArray.forEach(country => {
+      sendLikeRequest(country);
+    });
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
+
+// Function to send a POST request to the Involvement API for a specific country
+async function sendLikeRequest(country) {
+  try {
+    const response = await fetch(involvementApiEndpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        item_id: country.name
+      })
+    });
+
+    if (response.ok) {
+      console.log(`Liked ${country.name}`);
+    } else {
+      console.error(`Failed to like ${country.name}`);
+    }
+  } catch (error) {
+    console.error(`Error liking ${country.name}:`, error);
+  }
 }
-getlike();
+
