aa5947530112fab4b0bd26a15948e09cf2bc84d8
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-07-06 11:01:33 +0430

    Add test to comment counter

---
+++ src/modules/comment.js
@@ -0,0 +1,7 @@
+
+const getCommentCount = (commentsList) => {
+  const commentCount = commentsList.childElementCount;
+  return `Comment (${commentCount})`;
+};
+
+module.exports = { getCommentCount };
