3d24c7bc3b93eb7e9dc807c5d1455704c5147341
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-07-08 00:22:11 +0430

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,3 @@ import './modules/countries.js';
 import './modules/likeitems.js';
 import './modules/reservation.js';
 import './modules/commentpop.js';
-function updateCountryCounter() {
-  const countryCounter = document.querySelector('.country-counter');
-  const countryElements = document.querySelectorAll('.country');
-
-  countryCounter.textContent = countryElements.length.toString();
-}
-updateCountryCounter();
