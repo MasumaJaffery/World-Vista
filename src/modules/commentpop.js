c613cc7b90dc3cd23c8e8dcdd8d4247c13a27ab8
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-07-07 23:31:01 +0430

    Update commentpopup.js

--- src/modules/commentpop.js
+++ src/modules/commentpop.js
@@ -170,7 +170,8 @@ const openPopup = async (countryDetails) => {
     );
     const comments = await response.json();
     const commentCount = comments.length;
-    const commentText = `Comment (${commentCount})`;
+    const commentText =
+      commentCount > 0 ? `Comment (${commentCount})` : 'Comment (0)';
     commentsCount.textContent = commentText;
     comments.forEach((comment) => {
       displayComment(comment);
@@ -228,7 +229,7 @@ const openPopup = async (countryDetails) => {
         const comments = await commentsResponse.json();
         const commentCount = comments.length;
         const commentText =
-          commentCount === 0 ? 'Comment (0)' : `Comment (${commentCount})`;
+          commentCount > 0 ? `Comment (${commentCount})` : 'Comment (0)';
         commentsCount.textContent = commentText;
 
         // Clear the existing comments
